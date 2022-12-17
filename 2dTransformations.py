30e3b36c8cda706de345f05516a922f79ff9b06c
Sai Krishna <84342806+saikrshna@users.noreply.github.com>
2022-12-17 23:09:53 +0530

    Update 2dTransformations.py

--- 2dTransformations.py
+++ 2dTransformations.py
@@ -7,7 +7,7 @@ window_size=700
 
 def call_function(x1,y1,x2,y2,x3,y3):
     plot_triangle(x1,y1,x2,y2,x3,y3)
-    choice=int(input("Enter 1 for translation\nEnter 2 for rotation\nEnter 3 for Scaling\nEnter 4 for Reflection\nEnter 5 for Shearing: "))
+    choice=int(input("Enter 1 for translation\nEnter 2 for rotation\nEnter 3 for Scaling\nEnter 4 for Reflection\nEnter 5 for Shearing:"))
     if choice==1:
         translation(x1,y1,x2,y2,x3,y3)
     elif choice==2:
@@ -43,7 +43,7 @@ def rotation(x1,y1,x2,y2,x3,y3):
     y22=x2*math.sin(angle)+y2*math.cos(angle)
     x33=x3*math.cos(angle)-y3*math.sin(angle)
     y33=x3*math.sin(angle)+y3*math.cos(angle)
-    glColor3f(0.0,1.0,0.0)
+    glColor3f(0.0,1.0,0.1)
     glPointSize(10)
     glBegin(GL_TRIANGLES)
     glVertex2f(x11,y11)
@@ -53,8 +53,8 @@ def rotation(x1,y1,x2,y2,x3,y3):
     glFlush()
 
 def translation(x1,y1,x2,y2,x3,y3):
-    tx=float(input("Enter change in x: "))
-    ty=float(input("Enter change in y: "))
+    tx=float(input("Enter change in x:"))
+    ty=float(input("Enter change in y:"))
     glColor3f(0.0,0.0,1.0)
     glPointSize(10)
     glBegin(GL_TRIANGLES)
@@ -65,8 +65,8 @@ def translation(x1,y1,x2,y2,x3,y3):
     glFlush()
 
 def scaling(x1,y1,x2,y2,x3,y3):
-    sx=float(input("Enter scaling in x: "))
-    sy=float(input("Enter scaling in y: "))
+    sx=float(input("Enter scaling in x:"))
+    sy=float(input("Enter scaling in y:"))
     glColor3f(0.0,1.0,0.0)
     glPointSize(10)
     glBegin(GL_TRIANGLES)
@@ -124,7 +124,7 @@ def reflection(x1,y1,x2,y2,x3,y3):
 def shearing(x1,y1,x2,y2,x3,y3):
     c=int(input("Enter 1 for x shearing\nEnter 2 for y shearing\nEnter 3 for x-y shearing:"))
     if c==1:
-        shx=int(input("Enter shear for x: "))
+        shx=int(input("Enter shear for x:"))
         glColor3f(0.0,1.0,0.0)
         glPointSize(10)
         glBegin(GL_TRIANGLES)
@@ -134,7 +134,7 @@ def shearing(x1,y1,x2,y2,x3,y3):
         glEnd()
         glFlush()
     elif c==2:
-        shy=int(input("Enter shear for y: "))
+        shy=int(input("Enter shear for y:"))
         glColor3f(0.0,1.0,0.0)
         glPointSize(10)
         glBegin(GL_TRIANGLES)
@@ -144,8 +144,8 @@ def shearing(x1,y1,x2,y2,x3,y3):
         glEnd()
         glFlush()
     elif c==3:
-        shx=int(input("Enter shear for x: "))
-        shy=int(input("Enter shear for y: "))
+        shx=int(input("Enter shear for x:"))
+        shy=int(input("Enter shear for y:"))
         glColor3f(0.0,1.0,0.0)
         glPointSize(10)
         glBegin(GL_TRIANGLES)
@@ -158,13 +158,13 @@ def shearing(x1,y1,x2,y2,x3,y3):
 
 def main():
     glutInit()
-    x1=float(input("Enter x1 coordinate: "))
-    y1=float(input("Enter y1 coordinate: "))
-    x2=float(input("Enter x2 coordinate: "))
-    y2=float(input("Enter y2 coordinate: "))
-    x3=float(input("Enter x3 coordinate: "))
-    y3=float(input("Enter y3 coordinate: "))
-    glutCreateWindow("2D TRANSFROMATIONS")
+    x1=float(input("Enter x1 coordinate:"))
+    y1=float(input("Enter y1 coordinate:"))
+    x2=float(input("Enter x2 coordinate:"))
+    y2=float(input("Enter y2 coordinate:"))
+    x3=float(input("Enter x3 coordinate:"))
+    y3=float(input("Enter y3 coordinate:"))
+    glutCreateWindow("2D-TRANSFROMATIONS")
     glutInitWindowSize(window_size,window_size)
     glutDisplayFunc(lambda: call_function(x1,y1,x2,y2,x3,y3))
     ClearScreen()
